db94e4e089ecd6be15120ef9c4d07d60af4f3735
Luckysyama-2002 <ssundar4373@gmail.com>
2026-06-16 14:07:30 +0530

    useStateHook

--- concepts/src/topics/hooks/UseState_Hook.jsx
+++ concepts/src/topics/hooks/UseState_Hook.jsx
@@ -1,18 +1,26 @@
   
 import { useState } from "react"
+import { Child } from "./Child"
   const UseStateHook=()=>{
     //top level 
     // const [inital values, update function]=useState(initial value)
 
-    const [count,setCount] = useState(0)
+    // const [count,setCount] = useState(0)
+    // const updateCount=()=>{
+    //   setCount(count+1)
+    // }
+
+
+    const [count,setCount]=useState((0))            
     const updateCount=()=>{
       setCount(count+1)
     }
-
+    console.log("Parent component")
     return(
         <div>
         <h1>UseState Hook</h1>
         <h1>{count}</h1>
+        <Child/>
         <button onClick={updateCount}>Update</button>
         </div>
     )
